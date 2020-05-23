d2a6eac7e2e9a9f8152c81753004287d36b5d15f
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-05-23 23:38:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
+echo -e " \n \e[92m
+                  ___           ___           ___           ___           ___       ___ 
+      ___        /\__\         /\  \         /\  \         /\  \         /\__\     /\__\
+     /\  \      /::|  |       /::\  \        \:\  \       /::\  \       /:/  /    /:/  /
+     \:\  \    /:|:|  |      /:/\ \  \        \:\  \     /:/\:\  \     /:/  /    /:/  / 
+     /::\__\  /:/|:|  |__   _\:\~\ \  \       /::\  \   /::\~\:\  \   /:/  /    /:/  /  \e[36m
+  __/:/\/__/ /:/ |:| /\__\ /\ \:\ \ \__\     /:/\:\__\ /:/\:\ \:\__\ /:/__/    /:/__/   
+ /\/:/  /    \/__|:|/:/  / \:\ \:\ \/__/    /:/  \/__/ \/__\:\/:/  / \:\  \    \:\  \   
+ \::/__/         |:/:/  /   \:\ \:\__\     /:/  /           \::/  /   \:\  \    \:\  \  
+  \:\__\         |::/  /     \:\/:/  /     \/__/            /:/  /     \:\  \    \:\  \ 
+   \/__/         /:/  /       \::/  /                      /:/  /       \:\__\    \:\__\
+                 \/__/         \/__/                       \/__/         \/__/     \/__/ "          
 pkg install php php-apache wget -y
+echo -e " \e[93m klik izinkan "
 termux-setup-storage
-wget https://raw.githubusercontent.com/fdciabdul/InstagramAccountCreator/master/httpd.conf $PREFIX/etc/apache2
+wget --progress=bar:force https://raw.githubusercontent.com/fdciabdul/InstagramAccountCreator/master/httpd.conf $PREFIX/etc/apache2
 mkdir /sdcard/www
 apachectl
-print " Silahkan extract script yang sudah di kirimkan ke folder www di memori internal"
+echo " Silahkan extract script yang sudah di kirimkan ke folder www di memori internal"
