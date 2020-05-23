e37bdad26dfd463d3b3f1d1cb635ea9cecba3ecb
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-05-23 23:11:19 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+pkg install php php-apache wget -y
+termux-setup-storage
+wget https://raw.githubusercontent.com/fdciabdul/InstagramAccountCreator/master/httpd.conf $PREFIX/etc/apache2
+mkdir /sdcard/www
+apachectl
+print " Silahkan extract script yang sudah di kirimkan ke folder www di memori internal"
